79c88c928c66902da63eaf74e58ef8b6f640596b
Silas Cardoso <silasnunes105@gmail.com>
2025-03-10 12:39:08 -0300

    Primeiro commit

---
+++ src/main/java/api/igreja_on/IgrejaOnApplication.java
@@ -0,0 +1,13 @@
+package api.igreja_on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrejaOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgrejaOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/igreja_on/IgrejaOnApplicationTests.java
@@ -0,0 +1,13 @@
+package api.igreja_on;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgrejaOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
